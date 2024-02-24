4c0b5edd022dd2c8bb29ea4e37c0db446c6b05c6
YanaN <yn55510@gmail.com>
2024-02-24 14:04:17 +0200

    address review comments for hw01

--- src/main/java/ua/automation/hw1/Task1.java
+++ src/main/java/ua/automation/hw1/Task1.java
@@ -10,22 +10,21 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class Task1 {
+
     private WebDriver webDriver;
 
     @BeforeMethod
-    public void initDriver (){
+    public void initDriver() {
         webDriver = new ChromeDriver();
     }
 
     @AfterMethod
-    public void destroy(){
+    public void destroy() {
         webDriver.quit();
     }
 
     @Test
-    public void task1(){
-
-
+    public void task1() {
         webDriver.get("https://a-level.com.ua/");
         WebElement image = webDriver.findElement(By.xpath("/html/body/header/div[2]/div/div/div/a/img[1]"));
         Assert.assertTrue(image.isDisplayed(), "Page is not opened successfully");
